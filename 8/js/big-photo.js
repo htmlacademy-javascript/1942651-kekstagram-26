68617cace47a5b9e2415696f019d9a9f55c399c8
Keks <keks@htmlacademy.ru>
2022-07-25 02:53:41 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/big-photo.js
@@ -0,0 +1,82 @@
+const COMMENT_PER_STEP_COUNT = 5;
+const ESC_KEY_CODE = 27;
+
+const body = document.querySelector('body');
+const bigPictureContainer = document.querySelector('.big-picture');
+const closeButton = bigPictureContainer.querySelector('.big-picture__cancel');
+const imgElement = bigPictureContainer.querySelector('.big-picture__img img');
+const likesCountElement = bigPictureContainer.querySelector('.likes-count');
+const descriptionElement = bigPictureContainer.querySelector('.social__caption');
+const commentsListElement = bigPictureContainer.querySelector('.social__comments');
+const commentTemplate = commentsListElement.querySelector('.social__comment');
+const commentCountDivElement = bigPictureContainer.querySelector('.social__comment-count');
+const loadMoreCommentsButton = bigPictureContainer.querySelector('.comments-loader');
+
+
+const documentKeydownHandler = (evt) => {
+  if (evt.keyCode === ESC_KEY_CODE) {
+    evt.preventDefault();
+    closeBigPicture();
+  }
+};
+
+function closeBigPicture () {
+  bigPictureContainer.classList.add('hidden');
+  body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', documentKeydownHandler);
+}
+
+let renderedComments;
+let currentPicture;
+
+const renderComments = () => {
+  renderedComments  += COMMENT_PER_STEP_COUNT;
+  commentsListElement.innerHTML = '';
+
+  const { comments } = currentPicture;
+
+  const commentFragment = document.createDocumentFragment();
+  comments.slice(0, renderedComments).forEach(({avatar, name, message}) => {
+    const comment = commentTemplate.cloneNode(true);
+    const photoElement = comment.querySelector('.social__picture');
+    photoElement.src = avatar;
+    photoElement.alt = name;
+    comment.querySelector('.social__text').textContent = message;
+
+    commentFragment.append(comment);
+  });
+  commentsListElement.append(commentFragment);
+  if (comments.length <= renderedComments) {
+    commentCountDivElement.textContent = `${comments.length} из ${comments.length} комментариев`;
+    commentCountDivElement.classList.add('hidden');
+    loadMoreCommentsButton.classList.add('hidden');
+  } else {
+    commentCountDivElement.textContent = `${renderedComments} из ${comments.length} комментариев`;
+    loadMoreCommentsButton.classList.remove('hidden');
+  }
+};
+
+const loadMoreClickHandler = () => {
+
+
+  renderComments();
+};
+const openBigPhoto = (picture) => {
+  bigPictureContainer.classList.remove('hidden');
+  body.classList.add('modal-open');
+  imgElement.src = picture.url;
+  likesCountElement.textContent = picture.likes;
+  descriptionElement.textContent = picture.description;
+  renderedComments = 0;
+  currentPicture = picture;
+
+  renderComments();
+
+  closeButton.addEventListener('click', closeBigPicture);
+  loadMoreCommentsButton.addEventListener('click', loadMoreClickHandler);
+  document.addEventListener('keydown', documentKeydownHandler);
+};
+
+
+export {openBigPhoto};
---
+++ 8/js/effects.js
@@ -0,0 +1,145 @@
+const EFFECTS = {
+  none: {
+    filter: 'none',
+    range: {
+      min: 0,
+      max: 100,
+    },
+    step: 1,
+    start: 100,
+    unit : ''
+  },
+  chrome: {
+    filter: 'grayscale',
+    range: {
+      min: 0,
+      max: 1,
+    },
+    step: 0.1,
+    start: 1,
+    unit : '',
+    connect: 'lower'
+  },
+  sepia: {
+    filter: 'sepia',
+    range: {
+      min: 0,
+      max: 1,
+    },
+    step: 0.1,
+    start: 1,
+    unit : '',
+    connect: 'lower'
+  },
+  marvin: {
+    filter: 'invert',
+    range: {
+      min: 0,
+      max: 100,
+    },
+    step: 1,
+    start: 100,
+    unit : '%',
+    connect: 'lower'
+  },
+  phobos: {
+    filter: 'blur',
+    range: {
+      min: 0,
+      max: 3,
+    },
+    step: 0.1,
+    start: 3,
+    unit : 'px',
+    connect: 'lower'
+  },
+  heat: {
+    filter: 'brightness',
+    range: {
+      min: 1,
+      max: 3,
+    },
+    step: 0.1,
+    start: 3,
+    unit : '',
+    connect: 'lower'
+  }
+};
+
+const MIN_SCALE_RANGE = 25;
+const MAX_SCALE_RANGE = 100;
+const SCALE_STAPE = 25;
+
+const scaleContainer = document.querySelector('.img-upload__scale');
+const scaleElement = scaleContainer.querySelector('.scale__control--value');
+const photoElement = document.querySelector('.img-upload__preview img');
+const effectLevel = document.querySelector('.img-upload__effect-level');
+const effectValue = effectLevel.querySelector('.effect-level__value');
+const effectSlider = effectLevel.querySelector('.effect-level__slider');
+const effectList = document.querySelector('.effects__list');
+
+
+let scaleValue = MAX_SCALE_RANGE;
+
+
+const getScaleValue = (value) => {
+  scaleElement.value = `${value}%`;
+  photoElement.style.transform = `scale(${value * 0.01})`;
+};
+
+
+const clearScaleValue = () => {
+  scaleValue = MAX_SCALE_RANGE;
+  getScaleValue(MAX_SCALE_RANGE);
+  photoElement.style.transform = `scale(${scaleValue * 0.01})`;
+};
+
+
+const scaleClickHandler = (evt) => {
+  if (evt.target.matches('.scale__control--smaller') && scaleValue > MIN_SCALE_RANGE) {
+    scaleValue -= SCALE_STAPE;
+    return getScaleValue(scaleValue);
+  }
+
+  if (evt.target.matches('.scale__control--bigger') && scaleValue < MAX_SCALE_RANGE) {
+    scaleValue += SCALE_STAPE;
+    return getScaleValue(scaleValue);
+  }
+};
+
+getScaleValue(scaleValue);
+
+scaleContainer.addEventListener('click', scaleClickHandler);
+
+const resetEffect = () => {
+  photoElement.style.filter= '';
+  photoElement.className = '';
+  effectSlider.classList.add('hidden');
+};
+
+
+const effectChangeHandler = (evt) => {
+  const currentFilter = evt.target.value;
+  effectSlider.classList.remove('hidden');
+
+
+  if (effectSlider.noUiSlider) {
+    effectSlider.noUiSlider.destroy();
+  }
+
+  if (currentFilter === 'none') {
+    resetEffect();
+  } else {
+    noUiSlider.create(effectSlider, EFFECTS[currentFilter]);
+    photoElement.className = `effects__preview--${currentFilter}`;
+
+    effectSlider.noUiSlider.on('update', (values) => {
+      photoElement.style.filter = `${EFFECTS[currentFilter].filter}(${values}${EFFECTS[currentFilter].unit})`;
+      effectValue.value = parseFloat(values);
+    });
+  }
+};
+
+effectList.addEventListener('change', effectChangeHandler);
+
+export {clearScaleValue, resetEffect};
