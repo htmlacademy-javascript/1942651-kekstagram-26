65309b522941e23e046e782d2e6564b0b93c1397
SergeiMart <konoplyankas@mail.ru>
2022-07-25 20:59:00 +0300

    Фикс конфликта

--- js/api.js
+++ js/api.js
@@ -1,7 +1,10 @@
-import {showAlert} from './data.js';
+import {showAlert} from './util.js';
+
+const FETCH_DATA_URL = 'https://26.javascript.pages.academy/kekstagram/data';
+const SEND_DATA_URL = 'https://26.javascript.pages.academy/kekstagram';
 
 const getData = (onSuccess) => {
-  fetch('https://26.javascript.pages.academy/kekstagram/data')
+  fetch(FETCH_DATA_URL)
     .then((response) => response.json())
     .then((photo) => onSuccess(photo))
     .catch(() => {
@@ -11,7 +14,7 @@ const getData = (onSuccess) => {
 
 const sendData = (onSuccess, onFail, body) => {
   fetch(
-    'https://26.javascript.pages.academy/kekstagram',
+    SEND_DATA_URL,
     {
       method: 'POST',
       body,
